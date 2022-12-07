9dde7fd8f9992580c63069856c7536069030f3ee
Nick Baughman <baughmann1@gmail.com>
2022-12-07 06:50:38 -0500

    ditch unnecessary class

--- janusgraph-configuredgraphfactory-demo/src/main/kotlin/Main.kt
+++ janusgraph-configuredgraphfactory-demo/src/main/kotlin/Main.kt
@@ -4,38 +4,30 @@ import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection
 import org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource
 
+
 fun main(args: Array<String>) {
-    JanusGraphConfiguredGraphFactoryDemo().run()
+    val graphName = "myGraph"
+    val createANode = true
+
+    val g = openOrCreateGraph(graphName)
+    if (createANode) g.addV("person").property("name", "John").next()
+
+    println("Graph '$graphName' has ${g.V().count().next()} vertices and ${g.E().count().next()} edges")
+    println("Closing graph '$graphName'...")
+    g.close()
 }
 
-class JanusGraphConfiguredGraphFactoryDemo {
-    private fun openOrCreateGraph(graphName: String): GraphTraversalSource {
-        val cluster = Cluster.build("localhost").create()
-        val client = cluster.connect<Client.ClusteredClient>()
-        val existingGraphs = client.submit("ConfiguredGraphFactory.getGraphNames()").all().get()
-        val exists = existingGraphs.any { it.string == graphName }
-        if (!exists) {
-            println("Creating graph '$graphName'...")
-            client.submit("ConfiguredGraphFactory.create('$graphName')").all().get()
-        } else {
-            println("Graph '$graphName' already exists, opening...")
-        }
-        return traversal().withRemote(DriverRemoteConnection.using(cluster, "${graphName}_traversal"))
+private fun openOrCreateGraph(graphName: String): GraphTraversalSource {
+    val cluster = Cluster.build("localhost").create()
+    val client = cluster.connect<Client.ClusteredClient>()
+    val existingGraphs = client.submit("ConfiguredGraphFactory.getGraphNames()").all().get()
+    val exists = existingGraphs.any { it.string == graphName }
+    if (!exists) {
+        println("Creating graph '$graphName'...")
+        client.submit("ConfiguredGraphFactory.create('$graphName')").all().get()
+    } else {
+        println("Graph '$graphName' already exists, opening...")
     }
-
-    /**
-     * Creates a new graph with the given name, or open it if it already exists
-     *
-     * @param name the name of the graph to create
-     * @param createANode whether to create a node in the graph
-     */
-    fun run(graphName: String = "myNewGraph", createANode: Boolean = true) {
-        val g = openOrCreateGraph(graphName)
-        if (createANode) {
-            g.addV("person").property("name", "John").next()
-        }
-        println("Graph '$graphName' has ${g.V().count().next()} vertices and ${g.E().count().next()} edges")
-        println("Closing graph '$graphName'...")
-        g.close()
-    }
+    return traversal().withRemote(DriverRemoteConnection.using(cluster, "${graphName}_traversal"))
 }
+
